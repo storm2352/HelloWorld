3dbc88d50f8e00a4f9519d7a0468df0b5344df51
luke <storm2352@gmail.com>
2013-09-19 13:06:21 +0200

    Added Menu

--- src/com/helloworld/gwt/client/Home.java
+++ src/com/helloworld/gwt/client/Home.java
@@ -1,11 +1,11 @@
 package com.helloworld.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class Home implements EntryPoint {
 	
@@ -15,10 +15,28 @@ public class Home implements EntryPoint {
 	public void onModuleLoad() {
 		
 
-		DatePicker dp = new DatePicker();
-		RootPanel.get().add(dp);
+//		DatePicker dp = new DatePicker();
+//		RootPanel.get().add(dp);
+//		
+//		RootPanel.get().add(new TextBox());
+		MenuBar menu = new MenuBar();
+		Command cmd = new Command(){
+			public void execute(){
+				Window.alert("Click'd");
+			}
+		};
 		
-
+		MenuBar menuFile = new MenuBar(true /* vertical */);
+		
+		menuFile.addItem("New", cmd);
+		menuFile.addItem("Open", cmd);
+		menuFile.addItem("Close", cmd);
+		
+		menu.addItem(new MenuItem("File", menuFile)) ;
+		menu.addItem(new MenuItem("Edit", cmd));
+		menu.addItem(new MenuItem("Refactor", cmd));
+		
+		RootPanel.get().add(menu);
 	}
 
 }
